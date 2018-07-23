b270f6c6ebfc718d70091c62c503b15c914b3cae
Eric Zell <ezell@rbmtechnologies.com>
2018-07-23 18:17:42 -0400

    Add update state feature

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    e.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+  render() {
+    return (
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
+        <button type="submit">Visit Store ➡</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
